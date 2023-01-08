33b330be066f616b573307bad7e8d038c6e0db13
Roy Mogoko <RMogoko@auctionsplus.com.au>
2023-01-09 09:00:47 +1100

    Production api URL to qudos.app

---
+++ src/environments/dist/environment.prod.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.environment = void 0;
+exports.environment = {
+    production: true,
+    API_URL: 'https://qudos.app:3000',
+    // API_URL : 'http://146.190.193.83:3000',
+    INVITE_URL: 'https://qudos.app'
+};
