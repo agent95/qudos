c7a005adade5ae4154056dc416de370c72c09e33
Roy Mogoko <RMogoko@auctionsplus.com.au>
2023-01-10 11:10:07 +1100

    buld-275fdfd263c26a83

--- src/environments/dist/environment.prod.js
+++ src/environments/dist/environment.prod.js
@@ -3,7 +3,8 @@ exports.__esModule = true;
 exports.environment = void 0;
 exports.environment = {
     production: true,
-    API_URL: 'https://qudos.app:3000',
-    // API_URL : 'http://146.190.193.83:3000',
-    INVITE_URL: 'https://qudos.app'
+    // API_URL : 'https://qudos.app:3000',
+    API_URL: 'http://146.190.193.83:3000',
+    // INVITE_URL : 'https://qudos.app'
+    INVITE_URL: 'http://146.190.193.83'
 };
