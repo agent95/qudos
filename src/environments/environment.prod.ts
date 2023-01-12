5160800afc9043d5bcc2a2f4c4b0c6ded767b7c5
Roy Mogoko <RMogoko@auctionsplus.com.au>
2023-01-12 18:32:36 +1100

    Updated api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // API_URL : 'https://qudos.app:3000',
-  API_URL : 'http://146.190.193.83:3000',
-  // INVITE_URL : 'https://qudos.app'
-  INVITE_URL : 'http://146.190.193.83'
+  API_URL : 'https://qudos.app:3000',
+  // API_URL : 'http://146.190.193.83:3000',
+  INVITE_URL : 'https://qudos.app'
+  // INVITE_URL : 'http://146.190.193.83'
 };
